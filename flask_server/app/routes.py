0e0f930ca30e09e7c7632c32c0abbf945464ac60
sohampachpande <pachpande.soham@iitgn.ac.in>
2019-04-05 23:26:39 +0530

    babystep1

--- flask_server/app/routes.py
+++ flask_server/app/routes.py
@@ -1,8 +1,27 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
+
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='user',
+                             password='passwd',
+                             db='test',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Soham'}
     return render_template('index.html', title='CSE DB', user=user)
+
+
+@app.route('/form/', methods = ['POST'])
+def form():
+    name = request.form['Author']
+    year = request.form['Year']
+    s = "The name is: " + name + "\nYear is " + year
+    return s
