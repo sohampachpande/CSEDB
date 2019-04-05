ec67186e28a0833d585fa169f9d6d08eb704c64f
sohampachpande <pachpande.soham@iitgn.ac.in>
2019-04-05 19:37:35 +0530

    removed trash

--- django_project/project_mysite/app_csepub/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppCsepubConfig(AppConfig):
-    name = 'app_csepub'
--- django_project/project_mysite/project_mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('polls/', include('app_csepub.urls')),
-    path('admin/', admin.site.urls),
-]
