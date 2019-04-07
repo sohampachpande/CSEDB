c95b24a6cedb39012f094835c076c4fccc617304
sahitpj <jayakrishna.sahit@iitgn.ac.in>
2019-04-07 15:40:30 +0530

    added basic search-bar

--- flask_server/app/routes.py
+++ flask_server/app/routes.py
@@ -93,3 +93,24 @@ def get_author_page(author_id):
     # data = cursor.fetchone()
     print(l)
     return render_template('author_temp.html', author=l[0])
+
+
+
+@app.route('/search', methods = ['POST'])
+def search():
+    name = request.form['Author']
+    conference = request.form['Conference']
+    year = request.form['Year']
+    s = "The name is: " + name + "\nYear is " + year + conference
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+    # execute SQL query using execute() method.
+    cursor.execute('SELECT * FROM test_table ')
+    l = cursor.fetchall()
+
+    # Fetch a single row using fetchone()
+    # data = cursor.fetchone()
+    print(l)
+    print(s)
+    # disconnect from server
+    return s
